7b6603f68e304738b07a107e82297c483f093015
Sirajuddin Asjad <sirasjad@gmail.com>
2020-07-12 21:20:31 +0200

    Routing

---
+++ WebApp/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
