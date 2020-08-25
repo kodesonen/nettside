58594dc1e295834f049698fa5ded64e071822f25
Stian have <stianhave@hotmail.com>
2020-08-25 17:12:45 +0200

    Added Identity and EF stores

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -12,61 +12,79 @@ using Microsoft.EntityFrameworkCore;
 using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 using DatabaseHandler.Dataset;
 using System.IO;
+using WebApp.Models.Auth;
+using Microsoft.AspNetCore.Identity;
 
-namespace WebApp
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-            //DbConfig = new ConfigurationBuilder().AddJsonFile("connection.json").SetBasePath(Directory.GetCurrentDirectory()).Build();
-        }
+namespace WebApp {
 
-        public IConfiguration Configuration { get; }
-        //public IConfiguration DbConfig { get; }
+	public class Startup {
 
-        public void ConfigureServices(IServiceCollection services)
-        {
-            /*
-            services.AddDbContextPool<DataContext>(
-                options => options.UseMySql(Configuration.GetConnectionString("DefaultConnection")
-            ));*/
+		public Startup(IConfiguration configuration) {
+			Configuration = configuration;
+			//DbConfig = new ConfigurationBuilder().AddJsonFile("connection.json").SetBasePath(Directory.GetCurrentDirectory()).Build();
+		}
 
-            services.AddRouting(options => options.LowercaseUrls = true);
-            services.AddControllersWithViews();
-        }
+		public IConfiguration Configuration { get; }
+		//public IConfiguration DbConfig { get; }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-            //app.UseHttpsRedirection();
-            app.UseStaticFiles();
+		public void ConfigureServices(IServiceCollection services) {
+			/*
+			services.AddDbContextPool<DataContext>(
+				options => options.UseMySql(Configuration.GetConnectionString("DefaultConnection")
+			));*/
 
-            app.UseRouting();
+			services.AddRouting(options => options.LowercaseUrls = true);
+			services.AddControllersWithViews();
 
-            app.UseAuthorization();
+			services.AddIdentity<KodesonenUser, IdentityRole>(options => {
+				options.SignIn.RequireConfirmedEmail = false;
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+				options.Password.RequireNonAlphanumeric = false;
+				//options.User.RequireUniqueEmail = true;
+				//options.Lockout.AllowedForNewUsers = true;
+				//options.Lockout.MaxFailedAccessAttempts = 5;
+				//options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(1);
+			}).AddEntityFrameworkStores<DataContext>().AddDefaultTokenProviders();
 
-                endpoints.MapControllerRoute(
-                    name: "login",
-                    pattern: "login",
-                    defaults: new { controller = "User", action = "Login" });
-            });
-        }
-    }
+			services.ConfigureApplicationCookie(options => {
+				options.LoginPath = "/login";
+				options.ReturnUrlParameter = "";
+				options.AccessDeniedPath = "Error/404";
+			});
+			//services.Configure<DataProtectionTokenProviderOptions>(options => {
+			//    options.TokenLifespan = TimeSpan.FromMinutes(30);
+			//});
+
+			//services.Configure<EmailConfirmationTokenProviderOptions>(options => {
+			//    options.TokenLifespan = TimeSpan.FromDays(30);
+			//});
+		}
+
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+			if (env.IsDevelopment()) {
+				app.UseDeveloperExceptionPage();
+			} else {
+				app.UseExceptionHandler("/Home/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
+			//app.UseHttpsRedirection();
+			app.UseStaticFiles();
+
+			app.UseRouting();
+
+			app.UseAuthorization();
+
+			app.UseEndpoints(endpoints => {
+				endpoints.MapControllerRoute(
+					name: "default",
+					pattern: "{controller=Home}/{action=Index}/{id?}");
+
+				endpoints.MapControllerRoute(
+					name: "login",
+					pattern: "login",
+					defaults: new { controller = "User", action = "Login" });
+			});
+		}
+	}
 }
