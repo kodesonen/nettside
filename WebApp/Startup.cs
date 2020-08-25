84feeb7f128f13fbba126284275a408d92e2a01d
Stian have <stianhave@hotmail.com>
2020-08-25 17:09:49 +0200

    Dynamic changes made by extension

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -13,53 +13,60 @@ using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 using DatabaseHandler.Dataset;
 using System.IO;
 
-namespace WebApp {
+namespace WebApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            //DbConfig = new ConfigurationBuilder().AddJsonFile("connection.json").SetBasePath(Directory.GetCurrentDirectory()).Build();
+        }
 
-	public class Startup {
+        public IConfiguration Configuration { get; }
+        //public IConfiguration DbConfig { get; }
 
-		public Startup(IConfiguration configuration) {
-			Configuration = configuration;
-			//DbConfig = new ConfigurationBuilder().AddJsonFile("connection.json").SetBasePath(Directory.GetCurrentDirectory()).Build();
-		}
+        public void ConfigureServices(IServiceCollection services)
+        {
+            /*
+            services.AddDbContextPool<DataContext>(
+                options => options.UseMySql(Configuration.GetConnectionString("DefaultConnection")
+            ));*/
 
-		public IConfiguration Configuration { get; }
-		//public IConfiguration DbConfig { get; }
+            services.AddRouting(options => options.LowercaseUrls = true);
+            services.AddControllersWithViews();
+        }
 
-		public void ConfigureServices(IServiceCollection services) {
-			/*
-			services.AddDbContextPool<DataContext>(
-				options => options.UseMySql(Configuration.GetConnectionString("DefaultConnection")
-			));*/
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            //app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
-			services.AddRouting(options => options.LowercaseUrls = true);
-			services.AddControllersWithViews();
-		}
+            app.UseRouting();
 
-		public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
-			if (env.IsDevelopment()) {
-				app.UseDeveloperExceptionPage();
-			} else {
-				app.UseExceptionHandler("/Home/Error");
-				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-				app.UseHsts();
-			}
-			//app.UseHttpsRedirection();
-			app.UseStaticFiles();
+            app.UseAuthorization();
 
-			app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
 
-			app.UseAuthorization();
-
-			app.UseEndpoints(endpoints => {
-				endpoints.MapControllerRoute(
-					name: "default",
-					pattern: "{controller=Home}/{action=Index}/{id?}");
-
-				endpoints.MapControllerRoute(
-					name: "login",
-					pattern: "login",
-					defaults: new { controller = "User", action = "Login" });
-			});
-		}
-	}
+                endpoints.MapControllerRoute(
+                    name: "login",
+                    pattern: "login",
+                    defaults: new { controller = "User", action = "Login" });
+            });
+        }
+    }
 }
