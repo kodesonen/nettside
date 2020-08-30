67b4f080a842827d94086783118f135470ac63da
Stian have <stianhave@hotmail.com>
2020-08-30 21:12:13 +0200

    Register view and viewmodel

---
+++ WebApp/Models/Auth/RegisterModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models.Auth {
+
+	public class RegisterModel {
+
+		[Required]
+		public string Email { get; set; }
+
+		[Required]
+		[MinLength(2, ErrorMessage = "Minium required characters for first name: 2")]
+		public string FirstName { get; set; }
+
+		[MinLength(2, ErrorMessage = "Minium required characters for last name: 2")]
+		[Required]
+		public string LastName { get; set; }
+
+		[MinLength(5, ErrorMessage = "Minium required characters for password: 5")]
+		[Compare("ConfirmPassword", ErrorMessage = "Passwords does not match")]
+		public string Password { get; set; }
+
+		public string ConfirmPassword { get; set; }
+	}
+}
