6ead821ba8c043fc0f1650cb395ba07e37eff52b
Sirajuddin Asjad <sirasjad@gmail.com>
2020-10-11 01:37:26 +0200

    Fixed format

--- WebApp/Controllers/AuthenticationController.cs
+++ WebApp/Controllers/AuthenticationController.cs
@@ -9,91 +9,103 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using WebApp.Models.Auth;
 
-namespace WebApp.Controllers {
-	/*
+namespace WebApp.Controllers
+{
+    /*
 	 * TODO
 	 *	Add modelvalidators
 	 *  Add Model errors
 	 *	Add Validation summary
 	 */
 
-	public class AuthenticationController : Controller {
-		private readonly UserManager<KodesonenUser> userManager;
-		private readonly SignInManager<KodesonenUser> signInManager;
+    public class AuthenticationController : Controller
+    {
+        private readonly UserManager<KodesonenUser> userManager;
+        private readonly SignInManager<KodesonenUser> signInManager;
 
-		public AuthenticationController(
-			UserManager<KodesonenUser> userManager,
-			SignInManager<KodesonenUser> signInManager
-			) {
-			this.userManager = userManager;
-			this.signInManager = signInManager;
-		}
+        public AuthenticationController(
+            UserManager<KodesonenUser> userManager,
+            SignInManager<KodesonenUser> signInManager
+            )
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
 
-		#region login
+        #region login
 
-		[Route("Login")]
-		[HttpGet]
-		public IActionResult Login() {
-			return View();
-		}
+        [Route("Login")]
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
 
-		[Route("Login")]
-		[HttpPost]
-		public IActionResult Login(LoginModel model) {
-			if (!ModelState.IsValid) {
-				Console.WriteLine("Modelstate invalid");
-				return View(model);
-			}
+        [Route("Login")]
+        [HttpPost]
+        public IActionResult Login(LoginModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                Console.WriteLine("Modelstate invalid");
+                return View(model);
+            }
 
-			var result = signInManager.PasswordSignInAsync(model.Email, model.Password, true, false);
+            var result = signInManager.PasswordSignInAsync(model.Email, model.Password, true, false);
 
-			if (!result.Result.Succeeded) {
-				Console.WriteLine("Password does not match");
-				return View(model);
-			}
+            if (!result.Result.Succeeded)
+            {
+                Console.WriteLine("Password does not match");
+                return View(model);
+            }
 
-			return View();
-		}
+            return View();
+        }
 
-		#endregion login
+        #endregion login
 
-		#region register
+        #region register
 
-		[Route("Register")]
-		[HttpGet]
-		public IActionResult Register() {
-			return View();
-		}
+        [Route("Register")]
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
 
-		[Route("Register")]
-		[HttpPost]
-		public async Task<IActionResult> RegisterAsync(RegisterModel model) {
-			if (!ModelState.IsValid) {
-				Console.WriteLine("Modelstate is invalid");
-				return View(ModelState);
-			}
+        [Route("Register")]
+        [HttpPost]
+        public async Task<IActionResult> RegisterAsync(RegisterModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                Console.WriteLine("Modelstate is invalid");
+                return View(ModelState);
+            }
 
-			KodesonenUser user = new KodesonenUser() {
-				Id = Guid.NewGuid().ToString(),
-				Email = model.Email,
-				UserName = model.Email,
-				//Add first,lastname
-			};
+            KodesonenUser user = new KodesonenUser()
+            {
+                Id = Guid.NewGuid().ToString(),
+                Email = model.Email,
+                UserName = model.Email,
+                //Add first,lastname
+            };
 
-			var result = await userManager.CreateAsync(user, model.Password);
+            var result = await userManager.CreateAsync(user, model.Password);
 
-			if (!result.Succeeded) {
-				foreach (var error in result.Errors.ToList())
-					ModelState.AddModelError("All", error.Description.ToString());
-				Console.WriteLine("Error when creating user");
-				return View(model);
-			}
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors.ToList())
+                    ModelState.AddModelError("All", error.Description.ToString());
+                Console.WriteLine("Error when creating user");
+                return View(model);
+            }
 
-			await signInManager.PasswordSignInAsync(user.Email, model.Password, true, false);
-			Console.WriteLine("Registered!");
-			return View("Success");
-		}
+            await signInManager.PasswordSignInAsync(user.Email, model.Password, true, false);
+            Console.WriteLine("Registered!");
+            return View("Success");
+        }
 
-		#endregion register
-	}
+        #endregion register
+    }
 }
