adc9fa0357e4b7b05a7c574f8b4e015db7e776e7
Sirajuddin Asjad <sirasjad@gmail.com>
2020-10-18 18:55:40 +0200

    Reverted some changes

--- WebApp/Controllers/ChallengesController.cs
+++ WebApp/Controllers/ChallengesController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using DatabaseHandler.Models;
+using DatabaseHandler.Handlers;
 
 namespace WebApp.Controllers
 {
