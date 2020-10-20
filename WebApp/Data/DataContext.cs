a0ceef31db4aa456d6880dae4ed0567e7bf99bbe
Stian have <stianhave@hotmail.com>
2020-10-20 12:24:01 +0200

    Added datacontext and dataseeding

---
+++ WebApp/Data/DataContext.cs
@@ -0,0 +1,32 @@
+﻿using DatabaseHandler.Dataset;
+using DatabaseHandler.Models;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Data {
+
+	public class DataContext : DbContext {
+
+		public DataContext(DbContextOptions<DataContext> options) : base(options) {
+			Database.EnsureCreated();
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+			JObject data = JObject.Parse(File.ReadAllText("secrets.json"));
+			optionsBuilder.UseMySql(data["DefaultConnection"].ToString());
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder) {
+			modelBuilder.Seed();
+		}
+
+		public DbSet<User> Users { get; set; }
+
+		public DbSet<Challenge> Challenges { get; set; }
+	}
+}
