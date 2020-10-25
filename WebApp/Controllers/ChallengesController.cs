0e7614faa8a5eaa233d7ff1e94b31a98ecac5606
Sirajuddin Asjad <sirasjad@gmail.com>
2020-10-25 21:05:31 +0100

    Quick bugfix

--- WebApp/Controllers/ChallengesController.cs
+++ WebApp/Controllers/ChallengesController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using WebApp.DbHandler.Handlers;
 using WebApp.DbHandler.Models;
 using WebApp.DbHandler;
 using WebApp.DbHandler.Interfaces;
