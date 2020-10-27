b0af46b4261b191ef963af8a482891eac761cebf
Sirajuddin Asjad <sirasjad@gmail.com>
2020-10-27 18:43:11 +0100

    Added controller for Courses

--- WebApp/Controllers/CourseController.cs
+++ WebApp/Controllers/CourseController.cs
@@ -3,16 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebApp.DbHandler.Interfaces;
+using WebApp.DbHandler.Models;
 
 namespace WebApp.Controllers
 {
     public class CourseController : Controller
     {
+        private ICourseHandler CourseHandler;
+        public CourseController(ICourseHandler _ch)
+        {
+            this.CourseHandler = _ch;
+        }
+
         [HttpGet]
         [Route("Kurs")]
         public IActionResult Index()
         {
-            return View();
+            List<Course> CourseList = CourseHandler.LoadAll();
+            return View(CourseList);
         }
     }
 }
