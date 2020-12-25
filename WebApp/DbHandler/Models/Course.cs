87d4239b0fea400d2c97fe52107473ebe4cf75e6
Sirajuddin Asjad <sirasjad@gmail.com>
2020-12-25 23:47:25 +0100

    Quick fix, updated column from Title to Name

--- WebApp/DbHandler/Models/Course.cs
+++ WebApp/DbHandler/Models/Course.cs
@@ -7,7 +7,7 @@ namespace WebApp.DbHandler.Models
     {
         [Key]
         public int Id { get; set; }
-        public string Title { get; set; }
+        public string Name { get; set; }
         public string Description { get; set; }
         public string Icon { get; set; }
     }
