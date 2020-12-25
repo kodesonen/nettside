9cb3dde660de7d8c266ea5873c00677442cf9ea4
Sirajuddin Asjad <sirasjad@gmail.com>
2020-12-25 03:48:33 +0100

    Made a page for adding new courses

--- WebApp/Controllers/AdminController.cs
+++ WebApp/Controllers/AdminController.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebApp.DbHandler.Interfaces;
+using WebApp.DbHandler.Models;
 
 namespace WebApp.Controllers
 {
     public class AdminController : Controller
     {
+        private ICourseHandler CourseHandler;
+        public AdminController(ICourseHandler _ch)
+        {
+            this.CourseHandler = _ch;
+        }
+
         [HttpGet]
         [Route("admin")]
         public IActionResult Index()
@@ -18,6 +26,14 @@ namespace WebApp.Controllers
         [HttpGet]
         [Route("admin/kursbehandler")]
         public IActionResult ManageCourses()
+        {
+            List<Course> CourseList = CourseHandler.LoadAll();
+            return View(CourseList);
+        }
+
+        [HttpGet]
+        [Route("admin/kursbehandler/nytt-kurs")]
+        public IActionResult NewCourse()
         {
             return View();
         }
