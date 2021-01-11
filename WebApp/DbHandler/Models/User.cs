0f53f409f287f0f28a60594c479f81cdcfcddef7
Sirajuddin Asjad <sirasjad@gmail.com>
2021-01-11 23:36:34 +0100

    Minor adjustments to the user model

--- WebApp/DbHandler/Models/User.cs
+++ WebApp/DbHandler/Models/User.cs
@@ -4,8 +4,16 @@ namespace WebApp.DbHandler.Models
 {
 	public class User : IdentityUser
 	{
-		public string ApiKey { get; set; }
-		public string TestColumn { get; set; }
 		public bool Private { get; set; }
+		public string ApiKey { get; set; }
+
+		public string Description { get; set; }
+		public string StaffPosition { get; set; }
+
+		public string LinkedIn { get; set; }
+		public string GitHub { get; set; }
+
+		public string University { get; set; }
+		public string Study { get; set; }
 	}
 }
