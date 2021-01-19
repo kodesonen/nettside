c15fc79665ab1a246f0a40d828f81ac8b53a4364
Stian have <stianhave@hotmail.com>
2021-01-19 13:52:16 +0100

    Endret kontroller slik at vi kan bruke view til både oppretting og redigering av kurs

--- WebApp/Controllers/AdminController.cs
+++ WebApp/Controllers/AdminController.cs
@@ -8,89 +8,133 @@ using WebApp.DbHandler.Models;
 using WebApp.Models.Admin;
 using WebApp.Helpers;
 
-namespace WebApp.Controllers
-{
-    public class AdminController : Controller
-    {
-        private ICourseHandler CourseHandler;
-        public AdminController(ICourseHandler _ch)
-        {
-            this.CourseHandler = _ch;
-        }
+namespace WebApp.Controllers {
 
-        [HttpGet]
-        [Route("admin")]
-        public IActionResult Index()
-        {
-            return View();
-        }
+	public class AdminController : Controller {
+		private ICourseHandler CourseHandler;
 
-        [HttpGet]
-        [Route("admin/kursbehandler")]
-        public IActionResult ViewCourses()
-        {
-            List<Course> CourseList = CourseHandler.LoadAll();
-            return View(CourseList);
-        }
+		public AdminController(ICourseHandler _ch) {
+			this.CourseHandler = _ch;
+		}
 
-        [HttpGet]
-        [Route("admin/kursbehandler/nytt-kurs")]
-        public IActionResult NewCourse()
-        {
-            return View();
-        }
+		[HttpGet]
+		[Route("admin")]
+		public IActionResult Index() {
+			return View();
+		}
 
-        [HttpPost]
-        [Route("admin/kursbehandler/nytt-kurs")]
-        public IActionResult NewCourse(NewCourseModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                CourseHandler.AddNewCourse(model);
-                TempData["StatusMessage"] = StatusMessage.Success("Hurra!", "Du har lagt til et nytt kurs.");
-            }
-            else
-            {
-                TempData["StatusMessage"] = StatusMessage.Error("Oops!", "Vennligst fyll ut alle tekstfelt!");
-            }
-            return RedirectToAction("NewCourse");
-        }
+		[HttpGet]
+		[Route("admin/kursbehandler")]
+		public IActionResult ViewCourses() {
+			List<Course> CourseList = CourseHandler.LoadAll();
+			return View(CourseList);
+		}
 
-        [HttpGet]
-        [Route("admin/kursbehandler/behandle-kurs")]
-        public IActionResult ManageCourse(int id)
-        {
-            /* Check if ID attribute is unset */
-            if (id == 0) return RedirectToAction("ViewCourses");
+		/*
+		 *
+		 * Nytt kurs
+		 *
+		 */
 
-            /* Check if course name is null or invalid */
-            var courseName = CourseHandler.GetCourseNameById(id);
-            if (courseName != null) ViewBag.CourseName = courseName; 
-            else return RedirectToAction("ViewCourses");
+		[HttpGet]
+		[Route("admin/kursbehandler/nytt-kurs")]
+		public IActionResult NewCourse() {
+			return View();
+		}
 
-            /* Get all course modules */
-            List<Module> moduleList = CourseHandler.GetAllModulesById(id);
-            return View(moduleList);
-        }
+		//Måtte endre en del her, blir problematisk med to "identiske" modeller med en property i forskjell
+		[HttpPost]
+		[Route("admin/kursbehandler/nytt-kurs/{id?}")]
+		public IActionResult NewCourse(Course model) {
+			Console.WriteLine($"Model Id == {model.Id}");
+			if (model.Id == 0) {
+				//NYTT KURS
+				if (ModelState.IsValid) {
+					CourseHandler.AddNewCourse(model);
+					TempData["StatusMessage"] = StatusMessage.Success("Hurra!", "Du har lagt til et nytt kurs.");
+				} else {
+					TempData["StatusMessage"] = StatusMessage.Error("Oops!", "Vennligst fyll ut alle tekstfelt!");
+				}
+			} else {
+				if (ModelState.IsValid) {
+					CourseHandler.Update(model);
+					TempData["StatusMessage"] = StatusMessage.Success("Hurra!", "Kurset ble oppdatert");
+				} else {
+					TempData["StatusMessage"] = StatusMessage.Error("Oops!", "Noe gikk galt!");
+				}
+				Console.WriteLine("Oppdatert!");
+				return RedirectToAction("ManageCourse", model.Id);
+			}
+			return RedirectToAction("NewCourse");
+		}
 
-        [HttpGet]
-        [Route("admin/kursbehandler/endre-modul")]
-        public IActionResult EditModule(int kurs, int modul)
-        {
-            /* Check if the ID attributes are unset */
-            if (kurs == 0 || modul == 0) return RedirectToAction("ViewCourses");
+		/*
+		 *
+		 * Rediger kurs
+		 *
+		 */
 
-            /* Check if course name is null or invalid */
-            var courseName = CourseHandler.GetCourseNameById(kurs);
-            if (courseName != null) ViewBag.CourseName = courseName; 
-            else return RedirectToAction("ViewCourses");
+		[HttpGet]
+		[Route("admin/kursbehandler/nytt-kurs/{id}")]
+		public IActionResult NewCourse(int id) {
+			var course = CourseHandler.GetCourseById(id);
+			return View(course);
+		}
 
-            /* Check if module name is null or invalid */
-            var moduleName = CourseHandler.GetModuleNameById(modul);
-            if (moduleName != null) ViewBag.ModuleName = moduleName; 
-            else return RedirectToAction("ViewCourses");
+		//[HttpPost]
+		//[Route("admin/kursbehandler/nytt-kurs/{id}")]
+		//private IActionResult NewCourse(Course model) {
+		//	if (ModelState.IsValid) {
+		//		CourseHandler.Update(model);
+		//		TempData["StatusMessage"] = StatusMessage.Success("Hurra!", "Kurset ble oppdatert");
+		//	} else {
+		//		TempData["StatusMessage"] = StatusMessage.Error("Oops!", "Noe gikk galt!");
+		//	}
+		//	Console.WriteLine("Oppdatert!");
+		//	return RedirectToAction("ManageCourse", model.Id);
+		//}
 
-            return View();
-        }
-    }
+		[HttpGet]
+		[Route("admin/kursbehandler/behandle-kurs")]
+		public IActionResult ManageCourse(int id) {
+			/* Check if ID attribute is unset */
+			if (id == 0)
+				return RedirectToAction("ViewCourses");
+
+			/* Check if course name is null or invalid */
+			var courseName = CourseHandler.GetCourseNameById(id);
+			if (courseName != null)
+				ViewBag.CourseName = courseName;
+			else
+				return RedirectToAction("ViewCourses");
+
+			/* Get all course modules */
+			List<Module> moduleList = CourseHandler.GetAllModulesById(id);
+			return View(moduleList);
+		}
+
+		[HttpGet]
+		[Route("admin/kursbehandler/endre-modul")]
+		public IActionResult EditModule(int kurs, int modul) {
+			/* Check if the ID attributes are unset */
+			if (kurs == 0 || modul == 0)
+				return RedirectToAction("ViewCourses");
+
+			/* Check if course name is null or invalid */
+			var courseName = CourseHandler.GetCourseNameById(kurs);
+			if (courseName != null)
+				ViewBag.CourseName = courseName;
+			else
+				return RedirectToAction("ViewCourses");
+
+			/* Check if module name is null or invalid */
+			var moduleName = CourseHandler.GetModuleNameById(modul);
+			if (moduleName != null)
+				ViewBag.ModuleName = moduleName;
+			else
+				return RedirectToAction("ViewCourses");
+
+			return View();
+		}
+	}
 }
