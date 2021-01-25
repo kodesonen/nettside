5772bdb4d07458be8f08b46da38a5bbd7722a747
Stian have <stianhave@hotmail.com>
2021-01-25 19:34:41 +0100

    Renamed name to ModuleName

--- WebApp/DbHandler/DataSeeding.cs
+++ WebApp/DbHandler/DataSeeding.cs
@@ -2,36 +2,35 @@
 using Microsoft.EntityFrameworkCore;
 using WebApp.DbHandler.Models;
 
-namespace WebApp.DbHandler
-{
-    public static class ModelBuilderExtensions
-    {
-        public static void Seed(this ModelBuilder modelBuilder)
-        {
-            // Users
-            modelBuilder.Entity<User>().HasData(
-                new User { UserName = "Sirajuddin Asjad", Private = false },
-                new User { UserName = "Sirapudding", Private = true },
-                new User { UserName = "Stian Håve", Private = false }
-            );
+namespace WebApp.DbHandler {
 
-            // Challenges
-            modelBuilder.Entity<Challenge>().HasData(
-                new Challenge { Id = 1, Title = "Test challenge", Description = "This is a test!", Downloads = 0 },
-                new Challenge { Id = 2, Title = "Test challenge 2", Description = "This is a test!", Downloads = 99 }
-            );
+	public static class ModelBuilderExtensions {
 
-            // Courses
-            modelBuilder.Entity<Course>().HasData(
-                new Course { Id = 1, Name = "Test course", Description = "This is a test!", Icon = "fas fa-code" },
-                new Course { Id = 2, Name = "Another test", Description = "This is a test!", Icon = "fas fa-cogs" }
-            );
+		public static void Seed(this ModelBuilder modelBuilder) {
+			// Users
+			modelBuilder.Entity<User>().HasData(
+				new User { UserName = "Sirajuddin Asjad", Private = false },
+				new User { UserName = "Sirapudding", Private = true },
+				new User { UserName = "Stian Håve", Private = false }
+			);
 
-            // Modules
-            modelBuilder.Entity<Module>().HasData(
-                new Module { Id = 1, Name = "Test module", CourseId = 1, Chapter = 1, SubChapter = 1 },
-                new Module { Id = 2, Name = "Another module", CourseId = 1, Chapter = 1, SubChapter = 2 }
-            );
-        }
-    }
+			// Challenges
+			modelBuilder.Entity<Challenge>().HasData(
+				new Challenge { Id = 1, Title = "Test challenge", Description = "This is a test!", Downloads = 0 },
+				new Challenge { Id = 2, Title = "Test challenge 2", Description = "This is a test!", Downloads = 99 }
+			);
+
+			// Courses
+			modelBuilder.Entity<Course>().HasData(
+				new Course { Id = 1, Name = "Test course", Description = "This is a test!", Icon = "fas fa-code" },
+				new Course { Id = 2, Name = "Another test", Description = "This is a test!", Icon = "fas fa-cogs" }
+			);
+
+			// Modules
+			modelBuilder.Entity<Module>().HasData(
+				new Module { Id = 1, ModuleName = "Test module", CourseId = 1, Chapter = 1, SubChapter = 1 },
+				new Module { Id = 2, ModuleName = "Another module", CourseId = 1, Chapter = 1, SubChapter = 2 }
+			);
+		}
+	}
 }
