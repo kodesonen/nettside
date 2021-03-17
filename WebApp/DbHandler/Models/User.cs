343538c8ccbfb46e0c2e579e4ffe8b50aae508cc
Sirajuddin Asjad <sirasjad@gmail.com>
2021-03-18 00:47:08 +0100

    Updated profile page

--- WebApp/DbHandler/Models/User.cs
+++ WebApp/DbHandler/Models/User.cs
@@ -4,17 +4,22 @@ namespace WebApp.DbHandler.Models
 {
 	public class User : IdentityUser
 	{
+		/* Personal information */
 		public bool Private { get; set; }
 		public string UrlName { get; set; }
-		public string ApiKey { get; set; }
-
 		public string Description { get; set; }
 		public string StaffPosition { get; set; }
 
-		public string LinkedIn { get; set; }
-		public string GitHub { get; set; }
+		/* Social media */
+        public string Website { get; set; }
+        public string GitHub { get; set; }
+        public string LinkedIn { get; set; }
 
+		/* Study information */
 		public string University { get; set; }
 		public string Study { get; set; }
+
+		/* API information */
+		public string ApiKey { get; set; }
 	}
 }
--- WebApp/Models/User/ProfileModel.cs
+++ WebApp/Models/User/ProfileModel.cs
@@ -1,15 +1,26 @@
 using System;
+using System.Collections.Generic;
+using WebApp.DbHandler.Models;
 
 namespace WebApp.Models.User
 {
     public class ProfileModel
     {
+        /* Personal information */
         public string Name { get; set; }
         public string Description { get; set; }
 		public string StaffPosition { get; set; }
-		public string LinkedIn { get; set; }
-		public string GitHub { get; set; }
+		
+        /* Study information */
 		public string University { get; set; }
 		public string Study { get; set; }
+
+        /* Social media */
+        public string Website { get; set; }
+        public string GitHub { get; set; }
+        public string LinkedIn { get; set; }
+
+        /* Courses */
+        public List<Course> WrittenCourses { get; set; }
     }
 }
