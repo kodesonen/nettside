cb086caf216a4a1229b12842e577eb562f98df9d
Stian have <stianhave@hotmail.com>
2021-03-28 21:38:33 +0200

    Fix kursliste :)

--- WebApp/Controllers/CourseController.cs
+++ WebApp/Controllers/CourseController.cs
@@ -5,23 +5,53 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WebApp.DbHandler.Interfaces;
 using WebApp.DbHandler.Models;
+using WebApp.Models.Courses;
 
 namespace WebApp.Controllers
 {
-    public class CourseController : Controller
-    {
-        private ICourseHandler CourseHandler;
-        public CourseController(ICourseHandler _ch)
-        {
-            this.CourseHandler = _ch;
-        }
+	public class CourseController : Controller
+	{
+		private ICourseHandler CourseHandler;
+		private IUserHandler uh;
 
-        [HttpGet]
-        [Route("Kurs")]
-        public IActionResult Index()
-        {
-            List<Course> CourseList = CourseHandler.LoadAll();
-            return View(CourseList);
-        }
-    }
+		public CourseController(
+			ICourseHandler _ch,
+			IUserHandler uh
+			) {
+			this.CourseHandler = _ch;
+			this.uh = uh;
+		}
+
+		[Route("Kurs")]
+		public IActionResult Index() {
+			List<Course> CourseList = CourseHandler.LoadAll();
+			return View(CourseList);
+		}
+
+		[Route("Kurs/{id}")]
+		public IActionResult KursSide(int id) {
+			List<Module> ModuleList = CourseHandler.GetModulesByCourseId(id);
+			List<ModuleViewModel> model = new List<ModuleViewModel>();
+
+			foreach (var mod in ModuleList) {
+				ModuleViewModel vm = new ModuleViewModel();
+				vm.CourseId = id;
+				vm.courseName = CourseHandler.GetCourseNameById(id);
+				var usr = uh.GetUserById(vm.AuthorName);
+				vm.AuthorName = "Unknown";
+				if (usr != null) {
+					vm.AuthorName = usr.UserName;
+				}
+				vm.module = mod;
+				model.Add(vm);
+			}
+			return View(model);
+		}
+
+		[Route("Kurs/side/{id}")]
+		public IActionResult ModulSide(int id) {
+			Module model = CourseHandler.GetModuleById(id);
+			return View(model);
+		}
+	}
 }
--- WebApp/DbHandler/Models/Module.cs
+++ WebApp/DbHandler/Models/Module.cs
@@ -4,13 +4,13 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebApp.DbHandler.Models
 {
-    public class Module
-    {
-        [Key]
-        public int Id { get; set; }
-        public int CourseId { get; set; }
-        public int Chapter { get; set; }
-        public int SubChapter { get; set; }
+	public class Module
+	{
+		[Key]
+		public int Id { get; set; }
+		public int CourseId { get; set; }
+		public int Chapter { get; set; }
+		public int SubChapter { get; set; }
 		public string Author { get; set; }
 
 		/* Publish date and last updated */
@@ -18,13 +18,13 @@ namespace WebApp.DbHandler.Models
 		public DateTimeOffset Updated { get; set; }
 		public string LastUpdatedBy { get; set; }
 
-        // Navn på modul
-        public string ModuleName { get; set; }
-        public string Html { get; set; }
+		// Navn på modul
+		public string ModuleName { get; set; }
+		public string Html { get; set; }
 
-        // Brukes for å notifisere view om man skal oppdatere
-        // Eller lage ny modul
-        [NotMapped]
-        public bool update { get; set; }
-    }
+		// Brukes for å notifisere view om man skal oppdatere
+		// Eller lage ny modul
+		[NotMapped]
+		public bool update { get; set; }
+	}
 }
