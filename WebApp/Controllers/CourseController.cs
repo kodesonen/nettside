b76c7f5f968d5fca2777e153b77df3e51068b9d7
Sirajuddin Asjad <sirasjad@gmail.com>
2021-04-03 18:54:31 +0200

    Fixed routing and code formating

--- WebApp/Controllers/CourseController.cs
+++ WebApp/Controllers/CourseController.cs
@@ -9,49 +9,54 @@ using WebApp.Models.Courses;
 
 namespace WebApp.Controllers
 {
-	public class CourseController : Controller
-	{
-		private ICourseHandler CourseHandler;
-		private IUserHandler uh;
+    public class CourseController : Controller
+    {
+        private ICourseHandler CourseHandler;
+        private IUserHandler uh;
 
-		public CourseController(
-			ICourseHandler _ch,
-			IUserHandler uh
-			) {
-			this.CourseHandler = _ch;
-			this.uh = uh;
-		}
+        public CourseController(ICourseHandler _ch, IUserHandler uh)
+        {
+            this.CourseHandler = _ch;
+            this.uh = uh;
+        }
 
-		[Route("Kurs")]
-		public IActionResult Index() {
-			List<Course> CourseList = CourseHandler.LoadAll();
-			return View(CourseList);
-		}
+		[HttpGet]
+        [Route("kurs")]
+        public IActionResult Index()
+        {
+            List<Course> CourseList = CourseHandler.LoadAll();
+            return View(CourseList);
+        }
 
-		[Route("Kurs/{id}")]
-		public IActionResult KursSide(int id) {
-			List<Module> ModuleList = CourseHandler.GetModulesByCourseId(id);
-			List<ModuleViewModel> model = new List<ModuleViewModel>();
+		[HttpGet]
+        [Route("kurs/{id}")]
+        public IActionResult KursSide(int id)
+        {
+            List<Module> ModuleList = CourseHandler.GetModulesByCourseId(id);
+            List<ModuleViewModel> model = new List<ModuleViewModel>();
 
-			foreach (var mod in ModuleList) {
-				ModuleViewModel vm = new ModuleViewModel();
-				vm.CourseId = id;
-				vm.courseName = CourseHandler.GetCourseNameById(id);
-				var usr = uh.GetUserById(vm.AuthorName);
-				vm.AuthorName = "Unknown";
-				if (usr != null) {
-					vm.AuthorName = usr.UserName;
-				}
-				vm.module = mod;
-				model.Add(vm);
-			}
-			return View(model);
-		}
+            foreach (var mod in ModuleList)
+            {
+                ModuleViewModel vm = new ModuleViewModel();
+                vm.CourseId = id;
+                vm.courseName = CourseHandler.GetCourseNameById(id);
+                var usr = uh.GetUserById(vm.AuthorName);
+                vm.AuthorName = "Unknown";
 
-		[Route("Kurs/side/{id}")]
-		public IActionResult ModulSide(int id) {
-			Module model = CourseHandler.GetModuleById(id);
-			return View(model);
-		}
-	}
+                if (usr != null) vm.AuthorName = usr.UserName;
+                vm.module = mod;
+                model.Add(vm);
+            }
+			
+            return View(model);
+        }
+
+		[HttpGet]
+        [Route("kurs/side/{id}")]
+        public IActionResult ModulSide(int id)
+        {
+            Module model = CourseHandler.GetModuleById(id);
+            return View(model);
+        }
+    }
 }
--- WebApp/DbHandler/Models/Module.cs
+++ WebApp/DbHandler/Models/Module.cs
@@ -7,24 +7,24 @@ namespace WebApp.DbHandler.Models
 	public class Module
 	{
 		[Key]
-		public int Id { get; set; }
-		public int CourseId { get; set; }
-		public int Chapter { get; set; }
-		public int SubChapter { get; set; }
-		public string Author { get; set; }
+        public int Id { get; set; }
+        public int CourseId { get; set; }
+        public int Chapter { get; set; }
+        public int SubChapter { get; set; }
+        public string Author { get; set; }
 
-		/* Publish date and last updated */
-		public DateTimeOffset Published { get; set; }
-		public DateTimeOffset Updated { get; set; }
-		public string LastUpdatedBy { get; set; }
+        /* Publish date and last updated */
+        public DateTimeOffset Published { get; set; }
+        public DateTimeOffset Updated { get; set; }
+        public string LastUpdatedBy { get; set; }
 
-		// Navn på modul
-		public string ModuleName { get; set; }
-		public string Html { get; set; }
+        // Navn på modul
+        public string ModuleName { get; set; }
+        public string Html { get; set; }
 
-		// Brukes for å notifisere view om man skal oppdatere
-		// Eller lage ny modul
-		[NotMapped]
-		public bool update { get; set; }
-	}
+        // Brukes for å notifisere view om man skal oppdatere
+        // Eller lage ny modul
+        [NotMapped]
+        public bool update { get; set; }
+    }
 }
